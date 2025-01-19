21b4cedd6ce5cad9b026a9844923c6fe9351d6c2
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-01-19 07:28:16 +0200

    add CirclePlusSvg

---
+++ MyNewApp/icons/CirclePlusSvg.js
@@ -0,0 +1,13 @@
+import Svg, { Circle, Path } from 'react-native-svg';
+const CirclePlusSvg = (props) => (
+  <Svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
+    <Circle cx={12.5} cy={12.5} r={12} fill="#fff" stroke="#FF6C00" />
+    <Path
+      fill="#FF6C00"
+      fillRule="evenodd"
+      d="M13 6h-1v6H6v1h6v6h1v-6h6v-1h-6V6Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+);
+export default CirclePlusSvg;
